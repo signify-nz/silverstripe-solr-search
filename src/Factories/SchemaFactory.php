4e553763dd47d843fe181d3c48ed8c2660d41159
Morgan Middleton <morgan.middleton@signify.co.nz>
2024-12-09 10:24:23 +1300

    docs: update author attributions #7

--- src/Factories/SchemaFactory.php
+++ src/Factories/SchemaFactory.php
@@ -5,6 +5,8 @@
  * @package Firesphere\Solr\Search
  * @author Simon `Firesphere` Erkelens; Marco `Sheepy` Hermo
  * @copyright Copyright (c) 2018 - now() Firesphere & Sheepy
+ * @author Signify Ltd <info@signify.co.nz>
+ * Signify Ltd modified code in Nov 2024
  */
 
 namespace Firesphere\SolrSearch\Factories;
